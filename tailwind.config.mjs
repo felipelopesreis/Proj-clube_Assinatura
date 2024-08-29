bd35a499f005cf634cbfe01c3c94e64ff1915c1d
FELIPE LOPES DOS REIS <07041615162@ifms.edu.br>
2024-08-28 21:00:31 -0400

    implementando a estilização do projeto

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			transparent: 'transparent',
+			'pink-dark': '#7D298A',
+			'white': '#FFFFFF',
 		extend: {},
 	},
 	plugins: [],
+},
+	plugins: [
+		require('daisyui'),
+	],
 }
+
+
